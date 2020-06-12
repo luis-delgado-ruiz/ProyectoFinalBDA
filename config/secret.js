1f5b0767ffddbd77bdfe2fc9656f98231419387a
luis-delgado-ruiz <30873860+luis-delgado-ruiz@users.noreply.github.com>
2020-06-12 15:05:05 -0500

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,7 +1,7 @@
 module.exports = {
-    database: 'mongodb+srv://admin:qwerty123@cluster0-msv3b.mongodb.net/test?retryWrites=true&w=majority',
+    database: 'mongodb+srv://admin:@cluster0-msv3b.mongodb.net/test?retryWrites=true&w=majority',
     port: 3000,
-    secretKey:"test@dwlh"
+    secretKey:""
 }
 
 //aca se encuentra la informacion delicada para el acceso a la base de datos
